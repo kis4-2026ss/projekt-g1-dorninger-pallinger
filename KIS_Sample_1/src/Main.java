4f01885594495c17338eb338c790d6b4606cdaed
Joachim Pallinger <s2410307079@students.fh-hagenberg.at>
2026-05-21 16:26:16 +0200

    added sample project

---
+++ KIS_Sample_1/src/Main.java
@@ -0,0 +1,12 @@
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TodoModel model = new TodoModel();
+            TodoFrame frame = new TodoFrame(model);
+            frame.setVisible(true);
+        });
+    }
+}
---
+++ KIS_Sample_1/src/TodoItem.java
@@ -0,0 +1,27 @@
+
+public class TodoItem {
+    private final String text;
+    private boolean done;
+
+    public TodoItem(String text) {
+        this.text = text;
+        this.done = false;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void toggleDone() {
+        done = !done;
+    }
+
+    @Override
+    public String toString() {
+        return (done ? "[x] " : "[ ] ") + text;
+    }
+}
